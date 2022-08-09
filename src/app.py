5a4a6f7c93f76d5dfddd8ffee1ca73fabfe22228
mcecy <ceciliacadengue@gmail.com>
2022-08-09 12:59:29 -0300

    First commit

---
+++ src/app.py
@@ -0,0 +1,40 @@
+# pylint: disable=C0103,C0114,C0115,C0116
+import pwinput as pw
+
+palavra_secreta = pw.pwinput(prompt = 'Digite a palavra secreta: ')
+chances = 3
+digitadas = []
+
+while True:
+    if chances <= 0:
+        print('Você perdeu!')
+        break
+
+    guess = input('Digite uma letra: ')
+
+    if len(guess) > 1:
+        print('Não vale, digite só uma letra.')
+        continue
+
+    digitadas.append(guess)
+
+    palavra = ''
+
+    if guess not in palavra_secreta:
+        chances -= 1
+        print(f'Não foi dessa vez. Você tem {chances} chances.')
+        digitadas.pop()
+    else:
+        print('Muito bem! Você acertou!')
+
+    for letra in palavra_secreta:
+        if letra in digitadas:
+            palavra += letra
+        else:
+            palavra += '*'
+
+    print(palavra)
+
+    if palavra == palavra_secreta:
+        print(f'Você ganhou! A palavra era {palavra_secreta}.')
+        break
